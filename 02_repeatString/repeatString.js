c38b65ea5f6b74949aac44b1f9ee774453685842
akang1 <ajkang110@gmail.com>
2024-02-10 15:19:02 -0800

    completed exercises one and two

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let s = string;
+    let n = num;
+    let finalString = "";
 
+    if(n < 0){
+        return "ERROR";
+    }else{
+        for(let i = 0; i < n; i++){
+            finalString += s;
+        }
+    
+        return finalString;
+    }
 };
 
 // Do not edit below this line
